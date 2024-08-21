ecabfcaf37b915980a55b2537b7dca8a17d78b0a
u0928536 <architmenon.mnn@gmail.com>
2024-08-21 16:27:08 -0600

    augmented experience page

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,29 +5,47 @@ const items = [
     {
         id: 1,
         role: 'Software Engineer at Solventum (September 2024 - present)',
-        description: ['Stay tuned!']
+        description: ['Stay tuned!'],
+        image: 'solventum.png'
     },
     {
         id: 2,
         role: 'Full-Stack Engineer Freelance (October 2023 - present)',
         description: [
-            'Worked in a team of 3 with the California Collaborative for Educational Excellence (CCEE) to make web apps (React and AWS) for the California education system',
+            (<>
+                Contracted through <a href='https://www.inform2inspire.com/' className='text-blue-600 hover:text-blue-800 visited:text-purple-600'>Inform2Inspire</a>
+            </>),
+            'Worked in a team of 3 with the California Collaborative for Educational Excellence (CCEE) to make web apps for the California education system',
             "Created a hub for educational resources/initiatives with a lot functionality for admins, submitters, and the general public",
-            "Functionality includes searching, filtering, admin features, form submissions, authorization, tutorials, etc."
-        ]
+            "Functionality includes searching, filtering, admin features, form submissions, authorization, tutorials, etc.",
+        ],
+        image: 'i2i.png'
     },
     {
         id: 3,
         role: 'Teaching Assistant at the University of Utah School of Computing (January 2020 - May 2023)',
         description: [
             'Aided students in learning Java, Python, basic software engineering, debugging, sorting algorithms, graph algorithms, object-oriented programming, and data structures',
-            'Acted as a head TA for several courses which involved administrative duties as well as more teaching responsibility'
-        ]
+            'Acted as a head TA for several courses which involved administrative duties as well as more teaching responsibility',
+            (
+                <>
+                    Professors worked for:
+                    <span className='text-blue-600 hover:text-blue-800 visited:text-purple-600'>
+                        <a href='https://www.cs.utah.edu/~parker/'> Erin Parker</a>
+                        <a href='https://www.cs.utah.edu/~dkopta/'>, Daniel Kopta</a>
+                        <a href='https://www.cs.utah.edu/~dejohnso/'>, David Johnson</a>
+                        <a href='https://www.cs.utah.edu/~travis/'>, Travis Martin</a>
+                    </span>
+                </>
+            )
+        ],
+        image: 'utah.png'
     },
     {
         id: 4,
         role: 'Amazon Warehouse Associate (Summer 2019)',
-        description: ['Stowed a lot of product', 'Got a good workout']
+        description: ['Stowed a lot of product', 'Got a good workout'],
+        image: 'amazon.png'
     }
 ]
 
@@ -42,7 +60,15 @@ export default function Experience() {
                             <Disclosure key={item.id} as="div" className="pt-6">
                                 <dt>
                                     <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
-                                        <span className="text-base font-semibold leading-7">{item.role}</span>
+                                        <span className="text-base font-semibold leading-7">
+                                            {(item.image &&
+                                                <img
+                                                    alt=""
+                                                    src={process.env.PUBLIC_URL + '/' + item.image}
+                                                    className="inline-block h-14 w-14 rounded-md mr-4"
+                                                />)}
+                                            {item.role}
+                                        </span>
                                         <span className="ml-6 flex h-7 items-center">
                                             <PlusSmallIcon aria-hidden="true" className="h-6 w-6 group-data-[open]:hidden" />
                                             <MinusSmallIcon aria-hidden="true" className="h-6 w-6 [.group:not([data-open])_&]:hidden" />
